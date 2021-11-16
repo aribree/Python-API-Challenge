a4349acbdbf13433ab4c47b635f0417c7968b745
AriBree <89806020+aribree@users.noreply.github.com>
2021-11-16 14:07:42 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+# api_keys
+# weather_api_key
+weather_api_key = "edc82279e3a22bf537ebabb1d8c32e08"
+
+# Google API Key
+g_key = "AIzaSyC7n0kebwLoZIrmqZNACyUZWKkNBhJEeQg"
---
+++ starter_code/config.py
@@ -0,0 +1,8 @@
+# OpenWeatherMap API Key
+weather_api_key = "edc82279e3a22bf537ebabb1d8c32e08"
+
+# Google API Key
+g_key = "AIzaSyBPfsCLUw-OA9cfG8dlkSUUAIOgnYTukEo"
+
+#JSON Key
+JSON = "AIzaSyA9UD35DfJvvGNk9oevIBzpcc1rr75xq0s"
